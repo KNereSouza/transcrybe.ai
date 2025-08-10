66e695c3186c1656cf02f8f8dc3901f16f0422d7
KNereSouza <professional.neresk@gmail.com>
2025-08-10 11:42:38 -0300

    feat(routes): expõe endpoint GET /process para iniciar processamento via URL

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
+import VideoController from "../controllers/VideoController.js";
 
 const routes = Router();
+const videoController = new VideoController();
 
 routes.get("/", async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Hello World! Everything's okay.",
   });
+});
+
+routes.get("/process", async (req, res) => {
+  await videoController.handle(req, res);
 });
 
 export default routes;
